177b97bd0d40916bb23c275e453920a9b5364ac9
Javier Cerruti <javiercerruti@MacBook-Pro-de-Javier.local>
2024-06-14 14:35:12 -0300

    nuevos_cambios

--- components/video.tsx
+++ components/video.tsx
@@ -5,8 +5,8 @@ const Video = () => {
         <div className="container">
             <div className="videoContainer">
                 {/* <video className="bannerVideo" autoPlay={true} muted loop src="/videos/banner.mp4" typeof="video/mp4"></video> */}
-                <video className="bannerVideo d-none d-md-block" autoPlay={true} muted loop src="/videos/2024_06_13_banner1920x820.mp4" typeof="video/mp4"></video>
-                <video className="bannerVideo d-block d-md-none" autoPlay={true} muted loop src="/videos/2024_06_13_banner1920x900.mp4" typeof="video/mp4"></video>
+                <video className="bannerVideo d-none d-md-block" autoPlay={true} muted loop src="/videos/2024_06_14_banner1920x820.mp4" typeof="video/mp4"></video>
+                <video className="bannerVideo d-block d-md-none" autoPlay={true} muted loop src="/videos/2024_06_14_banner1920x900.mp4" typeof="video/mp4"></video>
             </div>
         </div>
     )
